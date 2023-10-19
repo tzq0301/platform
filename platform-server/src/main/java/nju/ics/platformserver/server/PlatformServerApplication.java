8afefafca5b0e08b822580ba09ddd4bfa7193797
Zhaoqi Tong <zhaoqi.tong@outlook.com>
2023-10-20 04:36:15 +0800

    rm server Dockerfile

--- platform-server/src/main/java/nju/ics/platformserver/server/PlatformServerApplication.java
+++ platform-server/src/main/java/nju/ics/platformserver/server/PlatformServerApplication.java
@@ -1,4 +1,4 @@
-package nju.ics.platformserver;
+package nju.ics.platformserver.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
