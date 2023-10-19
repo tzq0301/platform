b78ce17a1ad12f934aa1b31b9cf3d3347216829b
Zhaoqi Tong <zhaoqi.tong@outlook.com>
2023-10-20 00:51:44 +0800

    remove jar of server

---
+++ platform-server/src/main/java/nju/ics/platformserver/PlatformServerApplication.java
@@ -0,0 +1,11 @@
+package nju.ics.platformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformServerApplication.class, args);
+    }
+}
